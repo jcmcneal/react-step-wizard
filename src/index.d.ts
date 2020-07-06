ff0d2b3689c808a6e8a874d0bda22036e9f4284f
resolritter <joao.paulo.silvasouza@hotmail.com>
2020-07-06 12:28:22 -0300

    add TypeScript typings

---
+++ src/index.d.ts
@@ -0,0 +1,42 @@
+// Initial typings by dmk99 - https://github.com/jcmcneal/react-step-wizard/issues/31#issuecomment-505399131
+import * as React from "react"
+
+export type StepWizardProps = Partial<{
+  className: string
+
+  hashKey: string
+  initialStep: number
+  instance: (wizard: StepWizardProps) => void
+  isHashEnabled: boolean
+  isLazyMount: boolean
+  nav: JSX.Element
+
+  onStepChange: (stepChange: {
+    previousStep: number
+    activeStep: number
+  }) => void
+
+  transitions: {
+    enterRight?: string
+    enterLeft?: string
+    exitRight?: string
+    exitLeft?: string
+  }
+
+  children: JSX.Element[]
+}>
+
+export type StepWizardChildProps<T extends Record<string, any> = {}> = {
+  isActive: boolean
+  currentStep: number
+  totalSteps: number
+  firstStep: () => void
+  lastStep: () => void
+  nextStep: () => void
+  previousStep: () => void
+  goToStep: (step: number) => void
+  hashKey?: string
+} & T
+
+export declare function StepWizard(props: StepWizardProps): JSX.Element
+export default StepWizard
