353d5c99d6d0f888685520e4386e078bd210922c
Lucas Costa <lucascosta@Lucass-MBP.lan>
2021-04-23 13:38:13 -0500

    added ReactElement as a child option for ts type

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,42 +1,42 @@
 // Initial typings by dmk99 - https://github.com/jcmcneal/react-step-wizard/issues/31#issuecomment-505399131
-import * as React from "react"
+import * as React from "react";
 
 export type StepWizardProps = Partial<{
-  className: string
+  className: string;
 
-  hashKey: string
-  initialStep: number
-  instance: (wizard: StepWizardProps) => void
-  isHashEnabled: boolean
-  isLazyMount: boolean
-  nav: JSX.Element
+  hashKey: string;
+  initialStep: number;
+  instance: (wizard: StepWizardProps) => void;
+  isHashEnabled: boolean;
+  isLazyMount: boolean;
+  nav: JSX.Element;
 
   onStepChange: (stepChange: {
-    previousStep: number
-    activeStep: number
-  }) => void
+    previousStep: number;
+    activeStep: number;
+  }) => void;
 
   transitions: {
-    enterRight?: string
-    enterLeft?: string
-    exitRight?: string
-    exitLeft?: string
-  }
+    enterRight?: string;
+    enterLeft?: string;
+    exitRight?: string;
+    exitLeft?: string;
+  };
 
-  children: JSX.Element | JSX.Element[]
-}>
+  children: JSX.Element | JSX.Element[] | React.ReactElement;
+}>;
 
 export type StepWizardChildProps<T extends Record<string, any> = {}> = {
-  isActive: boolean
-  currentStep: number
-  totalSteps: number
-  firstStep: () => void
-  lastStep: () => void
-  nextStep: () => void
-  previousStep: () => void
-  goToStep: (step: number) => void
-  hashKey?: string
-} & T
+  isActive: boolean;
+  currentStep: number;
+  totalSteps: number;
+  firstStep: () => void;
+  lastStep: () => void;
+  nextStep: () => void;
+  previousStep: () => void;
+  goToStep: (step: number) => void;
+  hashKey?: string;
+} & T;
 
-export declare function StepWizard(props: StepWizardProps): JSX.Element
-export default StepWizard
+export declare function StepWizard(props: StepWizardProps): JSX.Element;
+export default StepWizard;
